91cc6ead049b694936d54c2f288898e3e8b21e2e
ih280 <ih280@cam.ac.uk>
2015-11-04 17:36:26 +0000

    Update lawrence_plot.py

--- lawrence_plot.py
+++ lawrence_plot.py
@@ -8,15 +8,17 @@
 # based on David Wade, May 2015
 # based on R script by Alex Archibald, February 2012
 #**********************************************************
+# Standard modules
 import datetime
 localtime = datetime.datetime.now().strftime("%Y_%m_%d")
-
 import numpy as np 
 import netCDF4 as ncdf
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib.colors as colors
+
+# Custom modules
 import lawrence_oh as l_oh
 
 #**********************************************************
@@ -24,10 +26,10 @@ jobid    = 'xjmxe'
 um       = '7.3'
 jobd     = ''
 print jobid
+nyrs     = 7 		# Allows to choose how many model years are to be averaged
 
-Na       = 6.022E23
-kgoh     = 17E-3
-
+Na       = 6.022E23	# Avogadro constant
+kgoh     = 17E-3	# molmass of OH
 #**********************************************************
 # Domain analysis
 # |  3  |  6  |  9  | 12  |     top hb[2]
@@ -45,19 +47,24 @@ hb       = [750,500,250]
 if   um == '7.3':
     volfile  = ncdf.Dataset('/tacitus/ih280/um/n48_l60_geovol.nc')
     lat      = volfile.variables['Lat'][:]
-#    lon      = volfile.variables['Lon'][:]
-#    lgt      = volfile.variables['z_theta'][:]
     vol      = volfile.variables['vol_theta'][:]
     vol      = vol[0]*1E6   # cm-3
 elif um == '8.4':
     volfile  = ncdf.Dataset('/tacitus/ih280/um/n96_l63_geovol.nc')
     lat      = volfile.variables['latitude'][:]
-#    lon      = volfile.variables['longitude'][:]
-#    hgt      = volfile.variables['hybrid_ht'][:]
     vol      = volfile.variables['vol_theta'][:]
     vol      = vol*1E6   # cm-3
 
-(oh,P,airmass) = l_oh.lawr(jobid)
+#**********************************************************
+# OH mixing ration, pressure and airmass data from model run
+(oh,P,airmass) = l_oh.lawr(jobid,nyrs)
+#**********************************************************
+# Tropospheric mask
+pclim      = np.empty(len(lat))
+l = 0
+for ilat in lat:
+  pclim[l] = 300 - 215 * (np.cos(ilat))**2
+  l += 1
 
 if   um == '7.3':
   mask     = np.zeros([60,73,96])
@@ -67,7 +74,7 @@ if   um == '7.3':
   for ihgt in range(0,60):
     for ilat in range(0,73):
         for ilon in range(0,96):
-            if P[ihgt,ilat,ilon]/100.0>= 200.0:
+            if P[ihgt,ilat,ilon]/100.0>= pclim[ilat]:
                 mask[ihgt,ilat,ilon] = 1
 elif um == '8.4':
   mask     = np.zeros([63,145,192])
@@ -77,7 +84,7 @@ elif um == '8.4':
   for ihgt in range(0,63):
     for ilat in range(0,145):
         for ilon in range(0,192):
-            if P[ihgt,ilat,ilon]/100.0>= 200.0:
+            if P[ihgt,ilat,ilon]/100.0>= pclim[ilat]:
                 mask[ihgt,ilat,ilon] = 1
 
 #**********************************************************
@@ -93,83 +100,24 @@ oh_zm    = np.mean  (oh_cm3, axis=2)
 zero     = np.zeros (oh_zm.shape)
 mass_zm  = np.mean  (airmass, axis=2)
 pres     = np.mean  (P, axis=2)/100.0 # convert to hPa
-
 #**********************************************************
 # AVERAGE OH INTO THE BOXES
-
-# pbox: list of 12 lists for the 12 atm boxes
-# nb of vert boxes * nb of horiz boxes
-# The first step will result in a nested array which then has to be flattened.
-pbox1    = [] 
-pbox2    = []
-pbox3    = []
-for i in range(len(hb)*(len(lb)-1)):
-    pbox1.append([])
-    pbox2.append([])
-    pbox3.append([])
-
-# boxes: list of 12 lists for the 12 atm boxes
-# nb of vert boxes * nb of horiz boxes
-# Flattened data from pbox1
-boxes    = pbox1[:]
-boxes2   = pbox2[:]
-boxmu    = pbox3[:]
-
-# Devide atmosphere into vertical boxes, spanning all latitudes
-# z contains indicies as a function of latitude for all pressure levels:
-# | z[2] | hb[1] > P >= hb[2]
-# | z[1] | hb[0] > P >= hb[1]
-# | z[0] | P >= hb[0]
-z        = [[],[],[]]
-for i in range(len(lat)):
-    z[0].append (np.where(  pres[:,i]>=hb[0] )                     [0])
-    z[1].append (np.where(( pres[:,i]>=hb[1] ) & (pres[:,i]<hb[0]))[0])
-    z[2].append (np.where(( pres[:,i]>=hb[2] ) & (pres[:,i]<hb[1]))[0])
-
-# Put OH into the boxes. 
-# This will result in a nested array, that then has to be flattened.
-# Use indices of z for the height boundaries.
-# Create latitude boundaries in loop.
-for i in range(len(lb)-1):              # number of lat boxes
-    latlow  = np.where (lat >= lb[i]   )[0][ 0]
-    lathigh = np.where (lat <  lb[i+1] )[0][-1]
-    for l in range(latlow,lathigh+1):   # lat slice
-        for h in range(len(hb)):        # number of vertical boxes
-            # convert OH to 10^6 molecules.cm-3!!!
-            pbox1[i*3+h].append (list( oh_zm   [z[h][l], l]*1E-6 ))
-            pbox2[i*3+h].append (list( mass_zm [z[h][l], l]      ))
-	    pbox3[i*3+h].append (list( zero    [z[h][l], l]      ))
-
-# Flatten lists in pbox1
-for i in range(len(hb)*(len(lb)-1)):
-    a=[]
-    b=[]
-    c=[]
-    for k in pbox1[i]:
-        for l in k:
-            a.append(l)
-    for k in pbox2[i]:
-        for l in k:
-            b.append(l)
-    for k in pbox3[i]:
-        for l in k:
-            c.append(l)
-    boxes [i] = np.array( a[:] ) # OH
-    boxes2[i] = np.array( b[:] ) # Mass
-    boxmu [i] = np.array( c[:] ) # Zeros
-
-del a, b, c, pbox1, pbox2, pbox3
+(databox, massbox, zerobox) = l_oh.boxes(pres,oh_zm,mass_zm,zero,lat,hb,lb)
+# databox: OH values that fall into the individual boxes
+# massbox: Airmass of those gridboxes that fall into the individual boxes
+# zerobox: Boxes that have the same shape as the data boxes;
+# zerobox: will contain the product of the airmass and OH
 
 # Calculate mean OH (and sd) for each box
 ohbox = np.empty(len(hb)*(len(lb)-1), dtype=np.float64)
 sdbox = np.empty(len(hb)*(len(lb)-1), dtype=np.float64)
 
 for i in range(len(hb)*(len(lb)-1)):
-    boxmu[i]  = boxes[i]*boxes2[i]
+    zerobox[i]  = databox[i]*massbox[i]
 
 for i in range(len(hb)*(len(lb)-1)):
-    ohbox[i] = "%.2f" %np.divide (np.mean( boxmu[i]),np.mean(boxes2[i]))
-    sdbox[i] = "%.2f" %np.std    (        boxes[i])
+    ohbox[i] = "%.2f" %np.divide (np.mean(zerobox[i]),np.mean(massbox[i])*1E-6) # air mass weighted OH in each box
+    sdbox[i] = "%.2f" %np.std    (        databox[i])
 
 #**********************************************************
 # Spivakovsky [OH]GM(M) *Lawrence et al 2001, ACP
